0730de82ec051cf83f7e22f70ad1f6c9eb94a2fb
KBDss <josren45@kingshigh.school.nz>
2023-08-22 12:22:26 +1200

    Added AI
    limbao

---
+++ WalkingSim/Enemy.gd
@@ -0,0 +1,25 @@
+extends CharacterBody3D
+
+@onready var nav_agent = $NavigationAgent3D
+@onready var player = get_node("/root/world/Player")
+
+var movement_speed = 1
+
+func _ready():
+	call_deferred("get_target")
+
+func get_target():
+	nav_agent.target_position = player.global_transform.origin
+
+func _physics_process(delta):
+	if nav_agent.is_navigation_finished():
+		get_target()
+	
+	var current_agent_position = global_position
+	var next_position = nav_agent.get_next_path_position()
+	var new_velocity = next_position - current_agent_position
+	new_velocity = new_velocity.normalized() * movement_speed
+	
+	velocity = new_velocity
+	move_and_slide()
+	
