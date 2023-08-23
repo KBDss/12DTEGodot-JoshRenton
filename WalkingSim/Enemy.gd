71f1a741be68694f35f2f00743c8d37f0cc55b11
KBDss <josren45@kingshigh.school.nz>
2023-08-23 13:19:53 +1200

    Added low resolution
    Made the game run in 640 x 360 and in fullscreen to add a retro effect

--- WalkingSim/Enemy.gd
+++ WalkingSim/Enemy.gd
@@ -3,23 +3,27 @@ extends CharacterBody3D
 @onready var nav_agent = $NavigationAgent3D
 @onready var player = get_node("/root/World/Player")
 
-var movement_speed = 1
+var movement_speed = 5
 
 func _ready():
 	call_deferred("get_target")
-
+	
 func get_target():
 	nav_agent.target_position = player.global_transform.origin
-
+	
 func _physics_process(delta):
+	look_at(player.global_position)
+	rotation_degrees.x = 0
+	rotation_degrees.z = 0
 	if nav_agent.is_navigation_finished():
 		get_target()
-	
-	var current_agent_position = global_position
+		
+	var current_position = global_position
 	var next_position = nav_agent.get_next_path_position()
-	var new_velocity = next_position - current_agent_position
+	var new_velocity = next_position - current_position
 	new_velocity = new_velocity.normalized() * movement_speed
 	
 	velocity = new_velocity
 	move_and_slide()
+		
 	
